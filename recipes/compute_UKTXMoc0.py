05010c1ff395165bd6be0f6d9e015a1a1d46e8d0
admin <admin>
2021-11-22 13:08:42 -0600

    Recipe FASHIONSEARCH.compute_UKTXMoc0 saved through notebook notebook editor for compute_UKTXMoc0

--- recipes/compute_UKTXMoc0.py
+++ recipes/compute_UKTXMoc0.py
@@ -1,12 +1,20 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+from transformers import AutoTokenizer, AutoModel
 
-
-
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
-model = dataiku.Folder("UKTXMoc0")
-model_info = model.get_info()
+model_folder = dataiku.Folder("UKTXMoc0")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/distilbert-base-nli-stsb-mean-tokens")
+model = AutoModel.from_pretrained("sentence-transformers/distilbert-base-nli-stsb-mean-tokens")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+tokenizer.save_pretrained(model_folder.get_path())
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+model.save_pretrained(model_folder.get_path())
