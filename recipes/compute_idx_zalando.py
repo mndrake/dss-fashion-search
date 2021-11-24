f83ef08118aad38e91cc0c6d84fd1c407231756d
admin <admin>
2021-11-24 11:22:34 -0600

    Saved recipe: FASHIONSEARCH.compute_idx_zalando

---
+++ recipes/compute_idx_zalando.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+image_details_by_image_path = dataiku.Dataset("image_details_by_image_path")
+image_details_by_image_path_df = image_details_by_image_path.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+idx_zalando_df = image_details_by_image_path_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+idx_zalando = dataiku.Dataset("idx_zalando")
+idx_zalando.write_with_schema(idx_zalando_df)
